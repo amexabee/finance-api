68732ba3401989113608bfc511eca48e90607e74
Amanuel Galema <amexabee@yahoo.com>
2023-12-08 10:11:05 +0200

    Update API for info

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -2,7 +2,8 @@ const corsOptions = {
   origin: (origin, callback) => {
     if (
       origin === 'http://localhost:3001' ||
-      origin === 'https://finance-gfwg.onrender.com'
+      origin === 'https://finance-gfwg.onrender.com' ||
+      !origin
     )
       callback(null, true);
     else callback(new Error('Not allowed by CORS'));
